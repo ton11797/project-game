d08addf273e8888e3fbeb9ba26abcc581ff204dd
DESKTOP-47CFN5D\Denice <ton11797@hotmail.com>
2016-10-29 13:06:38 +0700

    create menu

--- main.c
+++ main.c
@@ -4,9 +4,12 @@
 #include"gameprocess.h"
 void adddata(char*,char[],int);
 
-void main(){
-    int n,i,*status;
+int main(){
+    int n,i,*status,m;
     char *data,in[9];
+    m=menu();
+    system("cls");
+    if(m==1){
     scanf("%d",&n);
     data =(char*)malloc(n*n*9);
     status =(int*)malloc(sizeof(int)*(n*n));
@@ -17,6 +20,11 @@ void main(){
     render(n,data,status);
     free(data);
     free(status);
+    }else if(m==2){
+        printf("static");
+    }else{
+        return 0;
+    }
 }
 void adddata(char *data,char value[9],int order){
     int i;
@@ -25,4 +33,13 @@ void adddata(char *data,char value[9],int order){
     for(i=0;i<10;i++,data++){
         *(data) = value[i];
     }
+}
+int menu(){
+    int s;
+    printf("1.Start Game\n");
+    printf("2.Show score\n");
+    printf("3.Exit\n");
+    printf("Select 1-3 :");
+    scanf("%d",&s);
+    return s;
 }
