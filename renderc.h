94dadc0910d5be76576f783ab1b26f4d1f90624d
DESKTOP-47CFN5D\Denice <ton11797@hotmail.com>
2016-10-29 12:28:39 +0700

    add status 3 make card disaplear

--- renderc.h
+++ renderc.h
@@ -3,32 +3,54 @@
 void render(int n,char *data,int *s){
 	int i,j,k,v=1;
 	for (k = 0; k < n; ++k){
-		for (i = 0; i < n; ++i)printf("==========  ");
+        for (i = 0; i < n; ++i){
+                if(*s!=3)printf("==========  ");
+                if(*s==3)printf("            ");
+                s++;
+        }
+        s-=n;
 		printf("\n");
-        for(i=0;i<n;i++)printf("=        =  ");
+        for (i = 0; i < n; ++i){
+                if(*s!=3)printf("=        =  ");
+                if(*s==3)printf("            ");
+                s++;
+        }
+        s-=n;
 		printf("\n");
-		for(i=0;i<n;i++){
-            if(*s==1){
-                printf("=");
-                if(strlen(data)<8)for(j=0;j<(8-strlen(data))/2;j++)printf(" ");
-                printf("%s",data);
-                if(strlen(data)<8)for(j=0;j<(8-strlen(data))/2;j++)printf(" ");
-                if(strlen(data)%2!=0)printf(" ");
-                data+=9;
-                printf("=  ");
-                v++;
-            }else{
-                printf("=   %d   ",v);
-                if(v<9)printf(" ");
-                printf("=  ");
-                v++;
-            }
+            for(i=0;i<n;i++){
+                if(*s==1){
+                    printf("=");
+                    if(strlen(data)<8)for(j=0;j<(8-strlen(data))/2;j++)printf(" ");
+                    printf("%s",data);
+                    if(strlen(data)<8)for(j=0;j<(8-strlen(data))/2;j++)printf(" ");
+                    if(strlen(data)%2!=0)printf(" ");
+                    data+=9;
+                    printf("=  ");
+                    v++;
+                }else if(*s==0){
+                    printf("=   %d   ",v);
+                    if(v<9)printf(" ");
+                    printf("=  ");
+                    v++;
+                }else{
+                    printf("            ");
+                }
             s++;
-		}
+            }
 		printf("\n");
-        for(i=0;i<n;i++)printf("=        =  ");
+        s-=n;
+        for (i = 0; i < n; ++i){
+                if(*s!=3)printf("=        =  ");
+                if(*s==3)printf("            ");
+                s++;
+        }
 		printf("\n");
-		for (i = 0; i < n; ++i)printf("==========  ");
+        s-=n;
+        for (i = 0; i < n; ++i){
+                if(*s!=3)printf("==========  ");
+                if(*s==3)printf("            ");
+                s++;
+        }
         printf("\n");
 	}
 }
