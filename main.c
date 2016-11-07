f640f529b5a2d0b52c84e55359da67583e2d02c9
DESKTOP-47CFN5D\Denice <ton11797@hotmail.com>
2016-11-07 22:05:49 +0700

    add manu

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include"conio.h"
+#include<conio.h>
 #include<time.h>
 #include"renderc.h"
 #include"loadfile.h"
@@ -13,7 +13,7 @@ int manu();
 
 int main(){
     int n,i,*status,*ans,m,x,y,f=0,score;
-    char *data,in[(WORLDLEN+1)],name[30];
+    char *data,in[(WORLDLEN+1)],name[30],gc;
     while(1){
     n = manu();
     system("cls");
@@ -28,12 +28,12 @@ int main(){
         for(i=0;i<n*n;i++){
             *(status+i)=1;
         }
-        render(n,data,status);
+        render(n,data,status,score);
         delay(5000);
         for(i=0;i<n*n;i++){
             *(status+i)=0;
         }
-        render(n,data,status);
+        render(n,data,status,score);
         for(i=0;i<n*n;i++){
             printf("%d ",*(ans+i));
         }
@@ -47,11 +47,11 @@ int main(){
                 if(*(ans+x-1)==*(ans+y-1)){
                     *(status+x-1)=1;
                     *(status+y-1)=1;
-                    render(n,data,status);
+                    render(n,data,status,score);
                     delay(1000);
                     *(status+x-1)=3;
                     *(status+y-1)=3;
-                    render(n,data,status);
+                    render(n,data,status,score);
                     f++;
                     if(f>= n*n/2){
                         system("cls");
@@ -60,17 +60,17 @@ int main(){
                         scanf("%s",name);
                         savescore(name,score);
                         f=0;
-                        getch();
-                        break;}
+                        break;
+                        }
                 }else{
                     score-=100;
                     *(status+x-1)=1;
                     *(status+y-1)=1;
-                    render(n,data,status);
+                    render(n,data,status,score);
                     delay(3000);
                     *(status+x-1)=0;
                     *(status+y-1)=0;
-                    render(n,data,status);
+                    render(n,data,status,score);
                 }
             }else{
             printf("Try again!!\n");
@@ -79,10 +79,12 @@ int main(){
         free(data);
         free(status);
     }
+    if(n==99)break;
 }
     return 0;
 }
 int manu(){
+    system("cls");
     char title[6][100]={"     ________          ________          ________          ________     "
                          ,"    |\\   ____\\        |\\   __  \\        |\\   __  \\        |\\   ___ \\    "
                          ,"    \\ \\  \\___|        \\ \\  \\|\\  \\       \\ \\  \\|\\  \\       \\ \\  \\_|\\ \\   "
@@ -92,12 +94,49 @@ int manu(){
                          ,"        \\|_______|        \\|__|\\|__|        \\|__|\\|__|        \\|_______|"};
     int i,s=0;
     for(i=0;i<=6;i++)printf("%s\n",title[i]);
-        switch(s){
-            case 0:{
-                printf("%50s","press any key");
-            }
+    printf("%50s","press any key");
+    getch();
+    do{
+        system("cls");
+        for(i=0;i<=6;i++)printf("%s\n",title[i]);
+        printf("%45s","Select manu\n");
+        printf("%20s %s","","[1]Play Game\n");
+        printf("%20s %s","","[2]Show Score\n");
+        printf("%20s %s","","[3]Exit\n");
+        printf("%10s %s","","Enter number[1-3]");
+        scanf("%d",&i);
+        if(i==3){
+            return 99;
         }
-        return 2;
+        if(i==1){
+            system("cls");
+            for(i=0;i<=6;i++)printf("%s\n",title[i]);
+            printf("%45s","Select Level\n");
+            printf("%20s %s","","[1]Easy\n");
+            printf("%20s %s","","[2]Normal\n");
+            printf("%20s %s","","[3]Hard\n");
+            printf("%20s %s","","[4]Very Hard\n");
+            printf("%20s %s","","[99]EXIT\n");
+            printf("%10s %s","","Enter number[1-4,99]");
+            scanf("%d",&i);
+            if(i==99)return 99;
+            while(i>4||i<1){
+                printf("Enter again");
+                scanf("%d",&i);
+            }
+            return i*2;
+
+        }
+        if(i==2){
+            system("cls");
+            for(i=0;i<=6;i++)printf("%s\n",title[i]);
+            printf("%45s","Scoreboard\n");
+            readscore();
+            printf("%10s %s","","press any key back to menu");
+            getch();
+        }
+    }while(i!=99);
+    return 21;
 }
 void delay(unsigned int mseconds)
 {
